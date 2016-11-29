46991b5cc9d3861fd890864943fca5adbe84a60d
Taco <tvjohnson@gmail.com>
2016-11-29 14:28:33 -0600

    Some templates

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,18 @@
-alert('hello world 123');
+import { ProductListing } from './product-listing';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const html = ProductListing.render({
+        items: [
+            {
+                name: 'Thing 1',
+                code: 'thing-1'
+            },
+            {
+                name: 'Food 2',
+                code: 'food-2'
+            }
+        ]
+    });
+
+    document.getElementById('mount').innerHTML = html;
+});
